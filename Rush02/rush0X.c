3a25a2b95fcecd5c43040afa2f2472a58a808ab2
Pongkaseam Sumritpunsuk <ponsumri@z1t6c4.42bangkok.com>
2026-05-10 11:11:17 +0700

    five

--- Rush02/rush0X.c
+++ Rush02/rush0X.c
@@ -19,9 +19,23 @@ void	line_top(int x)
 	i = 0;
 	while (i < x)
 	{
-		if (i == 0)
+		if (i == 0 || i == x - 1)
 			ft_putchar('A');
-		else if (i == x - 1)
+		else
+			ft_putchar('B');
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	line_why(int x)
+{
+	int	i;
+
+	i = 0;
+	while (i < x)
+	{
+		if (i == 0 || i == x - 1)
 			ft_putchar('C');
 		else
 			ft_putchar('B');
@@ -55,8 +69,10 @@ void	rush(int x, int y)
 		return ;
 	while (aline < y)
 	{
-		if (aline == 0 || aline == y - 1)
+		if (aline == 0)
 			line_top(x);
+		else if (aline == y - 1)
+			line_why(x);
 		else
 			line_mid(x);
 		aline++;
